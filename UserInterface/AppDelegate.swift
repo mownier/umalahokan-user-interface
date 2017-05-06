aaf9e06cc01d62be6edc0d841b9636241aa4eeea
Mounir Ybanez <rinuom91@gmail.com>
2017-05-06 14:04:28 +0800

    Set LoginViewController as window's root.

--- UserInterface/AppDelegate.swift
+++ UserInterface/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var module: LoginModule!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        window?.rootViewController = LoginViewController()
+        FIRApp.configure()
+        module = LoginModule.create()
+        window?.rootViewController = module.scene as! LoginViewController
         return true
     }
 }
